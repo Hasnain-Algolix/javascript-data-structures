25f712306c953a54f15bfa34794b695828364e14
Hasnain-Algolix <130130503+Hasnain-Algolix@users.noreply.github.com>
2023-05-01 19:37:49 +0500

    Add reverse method

--- LinkedList.js
+++ LinkedList.js
@@ -158,6 +158,23 @@ class LinkedList {
     this.length--;
     return temp;
   }
+
+  // Reverse Method
+  reverse() {
+    let temp = this.head;
+    this.head = this.tail;
+    this.tail = temp;
+    let next = temp.next;
+    let prev = null;
+    for (let i = 0; i < this.length; i++) {
+      next = temp.next;
+      temp.next = prev;
+      prev = temp;
+      temp = next;
+    }
+
+    return this;
+  }
 }
 
 // Testing for Push()
