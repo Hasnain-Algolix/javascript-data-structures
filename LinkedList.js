20f2062a11fb14ec8dc1c83bc1c533ddd51fba28
Hasnain-Algolix <130130503+Hasnain-Algolix@users.noreply.github.com>
2023-05-01 19:39:12 +0500

    Add some basic methods

--- LinkedList.js
+++ LinkedList.js
@@ -8,6 +8,40 @@ class LinkedList {
     this.length = 1;
   }
 
+  printList() {
+    let temp = this.head;
+    while (temp !== null) {
+      console.log(temp.value);
+      temp = temp.next;
+    }
+  }
+
+  getHead() {
+    if (this.head === null) {
+      console.log('Head: null');
+    } else {
+      console.log('Head: ' + this.head.value);
+    }
+  }
+
+  getTail() {
+    if (this.tail === null) {
+      console.log('Tail: null');
+    } else {
+      console.log('Tail: ' + this.tail.value);
+    }
+  }
+
+  getLength() {
+    console.log('Length: ' + this.length);
+  }
+
+  makeEmpty() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
+
   // Add item to the end of the LinkedList
   push(value) {
     // Create new Node
