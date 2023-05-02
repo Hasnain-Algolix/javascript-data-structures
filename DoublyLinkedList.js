2c9dd12d68151ed10b8bbd078955d01365f43540
Hasnain-Algolix <130130503+Hasnain-Algolix@users.noreply.github.com>
2023-05-02 16:07:48 +0500

    Add Unshift method

--- DoublyLinkedList.js
+++ DoublyLinkedList.js
@@ -47,6 +47,21 @@ class DoublyLinkedList {
     this.length--;
     return temp;
   }
+
+  // Unshift Method (Add new item at start)
+  unshift(value) {
+    const newNode = new Node(value);
+    if (this.length === 0) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      newNode.next = this.head;
+      this.head.prev = newNode;
+      this.head = newNode;
+    }
+    this.length++;
+    return this;
+  }
 }
 
 const item = new DoublyLinkedList('Orange');
