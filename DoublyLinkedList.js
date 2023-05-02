5bb6062303be450c30771f51651cafaaf38acb70
Hasnain-Algolix <130130503+Hasnain-Algolix@users.noreply.github.com>
2023-05-02 13:51:13 +0500

    Add push method

--- DoublyLinkedList.js
+++ DoublyLinkedList.js
@@ -14,10 +14,27 @@ class DoublyLinkedList {
     this.tail = null;
     this.length = 0;
   }
+
+  // Push Method
+  push(value) {
+    const newNode = new Node(value);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      this.tail.next = newNode;
+      newNode.prev = this.tail;
+      this.tail = newNode;
+    }
+    this.length++;
+    return this;
+  }
 }
 
 const item = new DoublyLinkedList('Orange');
 item.makeEmtpy();
+item.push('Banana');
+item.push('Orange');
 console.log(item);
 
 module.exports = DoublyLinkedList;
