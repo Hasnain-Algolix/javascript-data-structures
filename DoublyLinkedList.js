7cf0e155a7f83607abdcdbb075ffbf902bb85172
Hasnain-Algolix <130130503+Hasnain-Algolix@users.noreply.github.com>
2023-05-02 12:59:27 +0500

    Add makeEmpty method

--- DoublyLinkedList.js
+++ DoublyLinkedList.js
@@ -7,9 +7,17 @@ class DoublyLinkedList {
     this.tail = newNode;
     this.length = 1;
   }
+
+  // Empty List
+  makeEmtpy() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
 }
 
 const item = new DoublyLinkedList('Orange');
+item.makeEmtpy();
 console.log(item);
 
 module.exports = DoublyLinkedList;
