dfc847424ceb3ce70ab603ed5357c2dbfb8ac6cf
Hasnain-Algolix <130130503+Hasnain-Algolix@users.noreply.github.com>
2023-05-09 12:40:58 +0500

    Create BST and Node class

---
+++ BST.js
@@ -0,0 +1,9 @@
+const Node = require('./Node');
+
+class BST {
+  constructor() {
+    this.root = null;
+  }
+}
+
+module.exports = BST;
--- index.js
+++ index.js
@@ -1,3 +1,4 @@
 const Node = require('./Node');
+const BST = require('./BST');
 
 console.log('🚀 Learning Data Structures in JavaScript');
