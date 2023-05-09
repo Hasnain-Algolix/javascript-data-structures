1f4f8ae7ee574a423928d0f156a0fc1f8db29a4e
Hasnain-Algolix <130130503+Hasnain-Algolix@users.noreply.github.com>
2023-05-09 16:21:34 +0500

    Add insert method in BST

--- BST.js
+++ BST.js
@@ -4,6 +4,48 @@ class BST {
   constructor() {
     this.root = null;
   }
+
+  insert(value) {
+    const newNode = new Node(value);
+
+    // Check if the tree is empty
+    if (this.root === null) {
+      this.root = newNode;
+      return this;
+    }
+
+    let temp = this.root;
+    while (true) {
+      if (newNode.value === temp.value) return undefined;
+      if (newNode.value < temp.value) {
+        if (temp.left === null) {
+          temp.left = newNode;
+          return this;
+        }
+        temp = temp.left;
+      } else {
+        if (newNode.value > temp.value) {
+          if (temp.right === null) {
+            temp.right = newNode;
+            return this;
+          }
+          temp = temp.right;
+        }
+      }
+    }
+  }
 }
+
+const myTree = new BST();
+
+myTree.insert(47);
+myTree.insert(21);
+myTree.insert(76);
+myTree.insert(18);
+
+myTree.insert(52);
+myTree.insert(82);
+
+console.log(myTree);
 
 module.exports = BST;
