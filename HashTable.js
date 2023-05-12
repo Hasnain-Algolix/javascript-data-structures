5ccbcef546e8eb71a7ab845894be5e86d94aa0a4
Hasnain-Algolix <130130503+Hasnain-Algolix@users.noreply.github.com>
2023-05-12 17:56:23 +0500

    Add get method for hash tables

--- HashTable.js
+++ HashTable.js
@@ -11,20 +11,42 @@ class HashTable {
     return hash;
   }
 
-  set(key, value) {
+  getIndex(key) {
     let index = this._hash(key);
+    return index;
+  }
+
+  set(key, value) {
+    const index = this.getIndex(key);
     if (!this.dataMap[index]) {
       this.dataMap[index] = [];
     }
     this.dataMap[index].push([key, value]);
     return this;
   }
+
+  get(key) {
+    // const index = this.getIndex(key);
+    let index = this._hash(key);
+
+    if (this.dataMap[index]) {
+      for (let i = 0; i < this.dataMap[index].length; i++) {
+        if (this.dataMap[index][i][0] === key) {
+          return this.dataMap[index][i][1];
+        }
+      }
+    }
+    return undefined;
+  }
 }
 
 const myHashTable = new HashTable();
-myHashTable.set('washers', 100);
-myHashTable.set('bolts', 100);
-myHashTable.set('lumber', 100);
+myHashTable.set('washers', 50);
+myHashTable.set('bolts', 1000);
+myHashTable.set('lumber', 20);
+
+// Use the get() method with all these keys
+console.log(myHashTable.get('bolts'));
 
 console.log(myHashTable);
 
