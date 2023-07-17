a5bf6e88e0a1f2bd80c592c252758c0c64ccb980
Hasnain-Algolix <130130503+Hasnain-Algolix@users.noreply.github.com>
2023-07-17 18:38:59 +0500

    add the merge sort algorithm

--- merge-sort/mergeSort.js
+++ merge-sort/mergeSort.js
@@ -28,3 +28,13 @@ const arr1 = [1, 4, 5, 7];
 const arr2 = [2, 3, 6, 8];
 const res = merge(arr1, arr2);
 console.log('result: ', res);
+
+function mergeSort(array) {
+  if (array.length === 1) return array;
+
+  let mid = Math.floor(array / 2);
+  let left = array.slice(0, mid);
+  let rigt = array.slice(mid);
+
+  return merge(mergeSort(left), mergeSort(right));
+}
