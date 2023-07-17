59fad45d7323c1950e70e538102bbfc258c77585
Hasnain-Algolix <130130503+Hasnain-Algolix@users.noreply.github.com>
2023-07-17 17:14:52 +0500

    create merge helper function

---
+++ merge-sort/mergeSort.js
@@ -0,0 +1,30 @@
+function merge(array1, array2) {
+  let combined = [];
+  let i = 0;
+  let j = 0;
+  while (i < array1.length && j < array2.length) {
+    if (array1[i] < array2[j]) {
+      combined.push(array1[i]);
+      i++;
+    } else {
+      combined.push(array2[j]);
+    }
+  }
+
+  while (i < array1.length) {
+    combined.push(array1[i]);
+    i++;
+  }
+
+  while (j < array1.length) {
+    combined.push(array2[j]);
+    j++;
+  }
+
+  return combined;
+}
+
+const arr1 = [1, 4, 5, 7];
+const arr2 = [2, 3, 6, 8];
+const res = merge(arr1, arr2);
+console.log('result: ', res);
