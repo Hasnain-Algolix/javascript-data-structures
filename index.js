9f1f42ef083e7f44899fac619e7ad79b7878397c
Hasnain-Algolix <130130503+Hasnain-Algolix@users.noreply.github.com>
2023-07-18 13:45:55 +0500

    update nodemon

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // require('./bubble-sort/bubbleSorting');
 // require('./selection-sort/selectionSort');
 // require('./insertion-sort/insertionSort');
-// require('./merge-sort/mergeSort');
+require('./merge-sort/mergeSort');
 
 console.log('🚀 Learning Data Structures in JavaScript');
--- merge-sort/mergeSort.js
+++ merge-sort/mergeSort.js
@@ -1,40 +1,40 @@
-// function merge(array1, array2) {
-//   let combined = [];
-//   let i = 0;
-//   let j = 0;
-//   while (i < array1.length && j < array2.length) {
-//     if (array1[i] < array2[j]) {
-//       combined.push(array1[i]);
-//       i++;
-//     } else {
-//       combined.push(array2[j]);
-//     }
-//   }
+function merge(array1, array2) {
+  let combined = [];
+  let i = 0;
+  let j = 0;
+  while (i < array1.length && j < array2.length) {
+    if (array1[i] < array2[j]) {
+      combined.push(array1[i]);
+      i++;
+    } else {
+      combined.push(array2[j]);
+    }
+  }
 
-//   while (i < array1.length) {
-//     combined.push(array1[i]);
-//     i++;
-//   }
+  while (i < array1.length) {
+    combined.push(array1[i]);
+    i++;
+  }
 
-//   while (j < array1.length) {
-//     combined.push(array2[j]);
-//     j++;
-//   }
+  while (j < array1.length) {
+    combined.push(array2[j]);
+    j++;
+  }
 
-//   return combined;
-// }
+  return combined;
+}
 
-// const arr1 = [1, 4, 5, 7];
-// const arr2 = [2, 3, 6, 8];
-// const res = merge(arr1, arr2);
-// console.log('result: ', res);
+const arr1 = [1, 4, 5, 7];
+const arr2 = [2, 3, 6, 8];
+const res = merge(arr1, arr2);
+console.log('result: ', res);
 
-// function mergeSort(array) {
-//   if (array.length === 1) return array;
+function mergeSort(array) {
+  if (array.length === 1) return array;
 
-//   let mid = Math.floor(array / 2);
-//   let left = array.slice(0, mid);
-//   let rigt = array.slice(mid);
+  let mid = Math.floor(array / 2);
+  let left = array.slice(0, mid);
+  let rigt = array.slice(mid);
 
-//   return merge(mergeSort(left), mergeSort(right));
-// }
+  return merge(mergeSort(left), mergeSort(right));
+}
